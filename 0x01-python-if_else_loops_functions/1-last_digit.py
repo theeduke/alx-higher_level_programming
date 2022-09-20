298df3abd072976147137d3f772cdf6bdee1d856
theeduke <oscarian528@gmail.com>
2022-09-20 13:33:28 -0700

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ rem = number %  10
 if rem > 5:
     print("Last digit of {} is {} and is greater than 5 \n".format(number, rem))
 
-if rem == 5:
-    print("Last digit of {} is {} and is greater than 5 \n".format(number, rem))
+if rem == 0:
+    print("Last digit of {} is {} and is 0 \n".format(number, rem))
 
 if rem < 6 and rem != 0: 
-    print("Last digit of {} is {} and is greater than 5 \n".format(number, rem))
+    print("Last digit of {} is {} and is less than 6 and not 0 \n".format(number, rem))
