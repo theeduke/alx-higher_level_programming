ac8cfb2eb2df0d82314a4e7992b6c7b816a163dd
theeduke <oscarian528@gmail.com>
2022-09-22 11:29:13 -0700

    print sum of two numbers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/bin/bash/python3
-from add_0 import add
-a = 1
-b = 2
+if __name__ == "__main__":
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+    from add_0 import add
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
