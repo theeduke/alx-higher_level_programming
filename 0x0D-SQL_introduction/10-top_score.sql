d50f963af34a223ebc7be60bf3066cb0b140ec43
theeduke <oscarian528@gmail.com>
2022-11-30 12:56:38 -0800

    getting info in descending order

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- list all records on 'second_table' starting with score then name in descending order
-SELECT (score, name) FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
