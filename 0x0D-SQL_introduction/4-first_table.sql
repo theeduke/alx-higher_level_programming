7fcfd9f132ffec61a57949c49b0521fcb6b68581
theeduke <oscarian528@gmail.com>
2022-11-30 12:02:36 -0800

    first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- create a new table' first_tablei in mysql server
-CREATE TABLE IF NOT EXISTS first_table('id'(INT), 'name'(VARCHAR(256)));
+CREATE TABLE IF NOT EXISTS first_table(id(INT), name(VARCHAR(256)));
